80508ffa6cb3b56a1a10bb0f7bde579e47cd5cac
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2024-03-29 21:41:32 +0000

    fix cors again 2

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use('/', routes);
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://houd.in'); // Update to match the domain you are making the request from
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
