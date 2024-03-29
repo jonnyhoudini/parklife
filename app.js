bca39b5edabcb6fcbd2b5beb66b67f02714e0cc9
jonnyhoudini <72469020+jonnyhoudini@users.noreply.github.com>
2024-03-29 22:04:02 +0000

    fix cors again 7

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
+// CORS Middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://houd.in'); // Update to match the domain you are making the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/', routes);
 
 // default catch all handler
